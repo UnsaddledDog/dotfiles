3c28c9a77b91bcd2cf047a2b9369d5408519a60f
UnsaddledDog <gergo.arendas@gmail.com>
2025-02-20 17:07:30 +0100

    added vim-help shell command

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -5,6 +5,7 @@
     enable = true;
     shellAliases = {
       rebuild = "darwin-rebuild switch --flake ~/nix-config/#darwin";
+      vim-help = "less ~/dotfiles/modules/neovim/help.txt";
     };
     oh-my-zsh = {
       enable = true;
@@ -14,7 +15,8 @@
   };
 
   # Ensure the custom theme file is placed in the correct location
-  home.file.".oh-my-zsh/custom/themes/alanpeabody-custom.zsh-theme".source = ./alanpeabody-custom.zsh-theme;
+  home.file.".oh-my-zsh/custom/themes/alanpeabody-custom.zsh-theme".source =
+    ./alanpeabody-custom.zsh-theme;
 
   home.sessionVariables = {
     # Set ZSH_CUSTOM to ensure Oh-My-Zsh picks up custom themes and plugins
